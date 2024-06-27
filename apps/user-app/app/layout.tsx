cb4b104d11a628d63f2ccb220d284d64d1804855
Nikhil chopra <nikhil0080012005@gmail.com>
2024-06-27 16:28:04 +0530

    Added docker file

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,10 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Providers } from "../provider";
 import { AppbarClient } from "../components/AppbarClient";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Wallet",
@@ -19,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>
+        <body>
           <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
             <AppbarClient />
             {children}
